d8d9e5a4a70c4800a26441f012ba00c639e5956d
呂枝益 <gte@GTE-MacBook-Pro.local>
2025-02-04 13:22:31 +0800

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import {Route, Routes} from 'react-router-dom'
 import Header from "./components/Header";
@@ -30,18 +30,16 @@ import SearchTutorial from "./pages/SearchTutorial";
 
 const App = () => {
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const params = new URLSearchParams(window.location.search);
-  //   const redirect = params.get("redirect");
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get("redirect");
 
-  //   console.log("Redirect parameter:", redirect);
-
-  //   if (redirect) {
-  //     navigate(redirect, { replace: true });
-  //   }
-  // }, [navigate]);
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [navigate]);
 
   return (
     
@@ -50,27 +48,27 @@ const App = () => {
         <CartProvider>
             <Header />
             <Routes>
-                <Route path="/coffee" element={<Home />} />
-                <Route path="/coffee/about/values" element={<Values />} />
-                <Route path="/coffee/about/story" element={<Story />} />
-                <Route path="/coffee/about/faq" element={<FAQ />} />
-                <Route path="/coffee/products" element={<Product />} />
-                <Route path="/coffee/products/hot" element={<Product />} />
-                <Route path="/coffee/products/coffee_gift_box" element={<Product />} />
-                <Route path="/coffee/products/single" element={<Product />} />
-                <Route path="/coffee/products/equipment" element={<Product />} />
-                <Route path="/coffee/products/featured" element={<Product />} />
-                <Route path="/coffee/cart" element={<Cart />} />
-                <Route path="/coffee/designer" element={<Designer />} />
-                <Route path="/coffee/portfolio" element={<LittleTales />} />
-                <Route path="/coffee/hamburger-turorial" element={<HamburgerMenuTutorial />} />
-                <Route path="/coffee/carousel-tutorial" element={<CarouselTutorial />} />
-                <Route path="/coffee/product-display-tutorial" element={<ProductDisplayTutorial />} />
-                <Route path="/coffee/product-view-tutorial" element={<ProductViewTutorial />} />
-                <Route path="/coffee/cart-tutorial" element={<CartTutorial />} />
-                <Route path="/coffee/modal-tutorial" element={<ModalTutorial />} />
-                <Route path="/coffee/search-tutorial" element={<SearchTutorial />} />
-                <Route path="/coffee/contact" element={<ContactForm />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/about/values" element={<Values />} />
+                <Route path="/about/story" element={<Story />} />
+                <Route path="/about/faq" element={<FAQ />} />
+                <Route path="/products" element={<Product />} />
+                <Route path="/products/hot" element={<Product />} />
+                <Route path="/products/coffee_gift_box" element={<Product />} />
+                <Route path="/products/single" element={<Product />} />
+                <Route path="/products/equipment" element={<Product />} />
+                <Route path="/products/featured" element={<Product />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/designer" element={<Designer />} />
+                <Route path="/portfolio" element={<LittleTales />} />
+                <Route path="/hamburger-turorial" element={<HamburgerMenuTutorial />} />
+                <Route path="/carousel-tutorial" element={<CarouselTutorial />} />
+                <Route path="/product-display-tutorial" element={<ProductDisplayTutorial />} />
+                <Route path="/product-view-tutorial" element={<ProductViewTutorial />} />
+                <Route path="/cart-tutorial" element={<CartTutorial />} />
+                <Route path="/modal-tutorial" element={<ModalTutorial />} />
+                <Route path="/search-tutorial" element={<SearchTutorial />} />
+                <Route path="/contact" element={<ContactForm />} />
                 <Route path="*" element={<Home />} />
             </Routes>
             <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import "../styles/footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-            <Link to="/coffee/portfolio"><span>關於這個作品</span></Link><span>︱</span>
-            <Link to="/coffee/designer"><span>關於作者</span></Link><span>︱</span>
-            <Link to="/coffee/contact"><span>與作者聯繫</span></Link>
+            <Link to="/portfolio"><span>關於這個作品</span></Link><span>︱</span>
+            <Link to="/designer"><span>關於作者</span></Link><span>︱</span>
+            <Link to="/contact"><span>與作者聯繫</span></Link>
             <div className="copyright"> © 2025 by xDesign</div>
         </div>
     )
